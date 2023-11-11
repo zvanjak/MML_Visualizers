035fc10f79899c23b74b56cb8608b9b39d1c4114
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-11 20:19:32 +0100

    daaa

--- MML_RealFunctionVisualizer/MainWindow.xaml.cs
+++ MML_RealFunctionVisualizer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,17 +25,44 @@ namespace MML_RealFunctionVisualizer
     {
       InitializeComponent();
 
-      double radius = 100;
-      double x = 200;
-      double y = 200;
+      var args = Environment.GetCommandLineArgs();
+
+      var fileName = args[1];
+
+      LoadData(fileName);
+
+      double radius = 10;
+      double x = 100;
+      double y = 100;
 
       Ellipse circle = new Ellipse();
       circle.Width = radius * 2;
       circle.Height = radius * 2;
-      circle.Fill = new SolidColorBrush(Colors.PaleVioletRed);
+      circle.Fill = new SolidColorBrush(Colors.Blue);
       mainCanvas.Children.Add(circle);
       Canvas.SetLeft(circle, x - radius);
       Canvas.SetTop(circle, y - radius);
+    }
+
+    public void LoadData(string inFileName)
+    {
+      string[] lines = File.ReadAllLines(inFileName);
+
+      foreach (var line in lines)
+      {
+        string[] parts = line.Split(' ');
+
+        double x = double.Parse(parts[0]);
+        double y = double.Parse(parts[1]);
+
+        Ellipse circle = new Ellipse();
+        circle.Width = 5;
+        circle.Height = 5;
+        circle.Fill = new SolidColorBrush(Colors.PaleVioletRed);
+        mainCanvas.Children.Add(circle);
+        Canvas.SetLeft(circle, x - 2.5);
+        Canvas.SetTop(circle, y - 2.5);
+      }
     }
   }
 }
