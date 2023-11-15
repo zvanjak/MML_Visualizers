372c912792cd601ae7b025380b33d726b3a96b32
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-15 21:32:11 +0100

    taaakooo

--- MML_ScalarFunction3Visualizer/MainWindow.xaml.cs
+++ MML_ScalarFunction3Visualizer/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,6 +21,19 @@ namespace MML_ScalarFunction3Visualizer
   /// </summary>
   public partial class MainWindow : Window
   {
+    PerspectiveCamera myPCamera = new PerspectiveCamera();
+
+    Point3D _cameraPos = new Point3D(180, 80, 150);
+    Point3D _lookToPos = new Point3D(0, 0, 0);
+
+    Point3D _startCameraPosRButtonClick;
+
+    private bool _bLButtonDown = false;
+    private bool _bRightButtonDown = false;
+
+    private Point _lastMousePos;
+    private Point _startMouseRButtonClick;
+
     public MainWindow()
     {
       InitializeComponent();
