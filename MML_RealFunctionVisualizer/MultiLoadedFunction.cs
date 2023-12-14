5032a54486235f182dfb51a941058572ac3a03b0
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-12-14 20:58:20 +0100

    eee

--- MML_RealFunctionVisualizer/MultiLoadedFunction.cs
+++ MML_RealFunctionVisualizer/MultiLoadedFunction.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace MML_RealFunctionVisualizer
 {
@@ -43,8 +44,31 @@ namespace MML_RealFunctionVisualizer
 
       for (int i = 0; i < _multiFuncX.Elements.Length; i++)
       {
+        // idemo po pojedinacnim funkcijama, odnosno njihovim redovim u matrici
         for (int j = 0; j < _multiFuncY.Rows; j++)
           Utils.DrawPoint(mainCanvas, coordSysParams, _multiFuncX.Elements[i], _multiFuncY.ElemAt(j, i), colors[j]);
+      }
+
+      for (int i = 0; i < _multiFuncX.Elements.Length-1; i++)
+      {
+        // idemo po pojedinacnim funkcijama, odnosno njihovim redovim u matrici
+        for (int j = 0; j < _multiFuncY.Rows; j++)
+        {
+          Line xAxis = new Line();
+          xAxis.Stroke = Brushes.Black;
+
+          double x1 = coordSysParams._centerX + _multiFuncX.Elements[i] * coordSysParams._scaleX;
+          double y1 = coordSysParams._centerY - _multiFuncY.ElemAt(j, i) * coordSysParams._scaleY;
+          double x2 = coordSysParams._centerX + _multiFuncX.Elements[i + 1] * coordSysParams._scaleX;
+          double y2 = coordSysParams._centerY - _multiFuncY.ElemAt(j, i+1) * coordSysParams._scaleY;
+
+          xAxis.X1 = x1;
+          xAxis.Y1 = y1;
+          xAxis.X2 = x2;
+          xAxis.Y2 = y2;
+
+          mainCanvas.Children.Add(xAxis);
+        }
       }
     }
   };
