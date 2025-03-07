62c7368ff3167e8793a19af2cf3ce0d0237536df
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2025-03-07 01:16:12 +0100

    eeeee

---
+++ MML_ParametricCurve2D_Visualizer/ParamCurve2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MML_ParametricCurve2D_Visualizer
+{
+  class ParamCurve2D
+  {
+    public List<double> _tVals = new List<double>();
+    public List<double> _xVals = new List<double>();
+    public List<double> _yVals = new List<double>();
+  }
+}
