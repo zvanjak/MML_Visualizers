cc362a6dc9daa40a7a6f18ca488b1ec7dc83f2a8
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2025-03-13 16:19:01 +0100

    eeee

--- WPF/MML_ParametricCurve2D_Visualizer/ParamCurve2D_MainWindow.xaml.cs
+++ WPF/MML_ParametricCurve2D_Visualizer/ParamCurve2D_MainWindow.xaml.cs
@@ -86,9 +86,9 @@ namespace MML_ParametricCurve2D_Visualizer
         for (int i = 5; i < lines.Length; i++)
         {
           string[] parts = lines[i].Split(' ');
-          newCurve._tVals.Add(double.Parse(parts[0]));
-          newCurve._xVals.Add(double.Parse(parts[1]));
-          newCurve._yVals.Add(double.Parse(parts[2]));
+          newCurve._tVals.Add(double.Parse(parts[0], CultureInfo.InvariantCulture));
+          newCurve._xVals.Add(double.Parse(parts[1], CultureInfo.InvariantCulture));
+          newCurve._yVals.Add(double.Parse(parts[2], CultureInfo.InvariantCulture));
         }
         _loadedCurves.Add(newCurve);
 
