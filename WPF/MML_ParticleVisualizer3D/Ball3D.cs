389567e9b1f8bb1fe754f5856f43ea204ae2be17
Zvonimr Vanjak <zvonimir.vanjak@gmail.com>
2025-04-29 09:38:05 +0200

    eee

---
+++ WPF/MML_ParticleVisualizer3D/Ball3D.cs
@@ -0,0 +1,40 @@
+﻿using MML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MML_ParticleVisualizer3D
+{
+  class Ball3D
+  {
+    private List<Vector3Cartesian> _vecPos = new List<Vector3Cartesian>();
+
+    private string _name;
+    private string _color;
+    private double _radius;
+
+    public string Name { get => _name; set => _name = value; }
+    public string Color { get => _color; set => _color = value; }
+    public double Radius { get => _radius; set => _radius = value; }
+
+    public Vector3Cartesian Pos(int i)
+    {
+      if (i < 0 || i >= _vecPos.Count)
+        throw new IndexOutOfRangeException("Index out of range in Ball.Pos");
+      return _vecPos[i];
+    }
+    public void AddPos(Vector3Cartesian pos)
+    {
+      _vecPos.Add(pos);
+    }
+
+    public Ball3D(string name, string color, double radius)
+    {
+      _name = name;
+      _color = color;
+      _radius = radius;
+    }
+  }
+}
--- WPF/MML_ParticleVisualizer3D/ParticleVisualizer3D_MainWindow.xaml.cs
+++ WPF/MML_ParticleVisualizer3D/ParticleVisualizer3D_MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using MML;
+using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -75,14 +77,94 @@ namespace MML_ParticleVisualizer3D
       //Utils.DrawCoordSystem(_myModel3DGroup, _lineWidth * 3, _axisLen);
 
 
-      for (int i = 0; i < _curves.Count; i++)
-      {
-        MeshGeometry3D line = Geometries.CreatePolyLine(_curves[i]._curveTrace, _lineWidth, 10);
-        DiffuseMaterial lineMaterial = new DiffuseMaterial(_brushes[i]);
-        GeometryModel3D lineModel = new GeometryModel3D(line, lineMaterial);
+      //for (int i = 0; i < _curves.Count; i++)
+      //{
+      //  MeshGeometry3D line = Geometries.CreatePolyLine(_curves[i]._curveTrace, _lineWidth, 10);
+      //  DiffuseMaterial lineMaterial = new DiffuseMaterial(_brushes[i]);
+      //  GeometryModel3D lineModel = new GeometryModel3D(line, lineMaterial);
 
-        _myModel3DGroup.Children.Add(lineModel);
+      //  _myModel3DGroup.Children.Add(lineModel);
+      //}
+    }
+
+    public bool LoadData(string fileName)
+    {
+      // get number of balls
+      int numBalls = 0;
+      string[] lines = System.IO.File.ReadAllLines(fileName);
+
+      if (lines[0] == "PARTICLE_SIMULATION_DATA_2D")
+      {
+        // read the number of balls
+        string[] parts = lines[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length != 2)
+          throw new Exception("Invalid number of balls in SimData.txt");
+
+        numBalls = int.Parse(parts[1]);
+
+        int lineNumber = 2;
+
+        // read the balls attributes - name, radius, type
+        for (int i = 0; i < numBalls; i++)
+        {
+          parts = lines[lineNumber++].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+          if (parts.Length != 3)
+            throw new Exception("Invalid ball attributes in SimData.txt");
+
+          string name = parts[0];
+          string color = parts[1];
+          double radius = double.Parse(parts[2], CultureInfo.InvariantCulture);
+          Ball ball = new Ball(name, color, radius);
+
+          _balls.Add(ball);
+        }
+
+        // read number of steps
+        parts = lines[lineNumber++].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 2)
+          throw new Exception("Invalid number of steps in SimData.txt");
+        int numSteps = int.Parse(parts[1]);
+
+        _numSteps = numSteps;
+
+        // read the steps
+        for (int i = 0; i < numSteps; i++)
+        {
+          // we expect "Step 0" together with timing (ie, additional double to specify T)
+          parts = lines[lineNumber++].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+          if (parts.Length != 3)
+            throw new Exception("Invalid number of steps in SimData.txt");
+
+          if (parts[0] != "Step")
+            throw new Exception("Invalid step in SimData.txt");
+          if (parts[1] != i.ToString())
+            throw new Exception("Invalid step number in SimData.txt");
+          double T = double.Parse(parts[2], CultureInfo.InvariantCulture);
+
+          // read the positions of the balls
+          for (int j = 0; j < numBalls; j++)
+          {
+            parts = lines[lineNumber++].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 3)
+              throw new Exception("Invalid ball position coords in SimData.txt, line number - " + lineNumber.ToString());
+
+            int index = int.Parse(parts[0]);
+            double x = double.Parse(parts[1], CultureInfo.InvariantCulture);
+            double y = double.Parse(parts[2], CultureInfo.InvariantCulture);
+
+            Vector2Cartesian pos = new Vector2Cartesian(x, y);
+
+            _balls[j].AddPos(pos);
+          }
+        }
       }
+      else
+      {
+      }
+
+      return true;
     }
   }
 }
