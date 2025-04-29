f53bad723ec90575855f93b354aa7f84fffbb86e
Zvonimr Vanjak <zvonimir.vanjak@gmail.com>
2025-04-29 09:26:40 +0200

    eeee

--- WPF/MML_ParticleVisualizer3D/ParticleVisualizer3D_MainWindow.xaml.cs
+++ WPF/MML_ParticleVisualizer3D/ParticleVisualizer3D_MainWindow.xaml.cs
@@ -6,8 +6,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF3DHelperLib;
 
 namespace MML_ParticleVisualizer3D
 {
@@ -16,6 +18,10 @@ namespace MML_ParticleVisualizer3D
   /// </summary>
   public partial class ParticleVisualizer3D_MainWindow : Window
   {
+    readonly WorldCameraMouseHelper _helper = new WorldCameraMouseHelper();
+
+    Model3DGroup _myModel3DGroup = new Model3DGroup();
+
     public ParticleVisualizer3D_MainWindow()
     {
       InitializeComponent();
