d48d03fca96c6d8f2ed2a870587b76259f94ffc6
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2025-07-03 16:32:50 +0200

    eeee

---
+++ WPF/MML_RealFunctionVisualizer/LineColors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MML_RealFunctionVisualizer
+{
+  // containes Brush instances for drawing lines
+  internal static class LineBrushes
+  {
+    public static readonly Brush[] brushes = new Brush[]
+    {
+    Brushes.Black,
+    Brushes.Blue,
+    Brushes.Red,
+    Brushes.Green,
+    Brushes.Orange,
+    Brushes.Purple,
+    Brushes.Brown,
+    Brushes.Cyan,
+    Brushes.Magenta,
+    Brushes.Gray
+    };
+
+  }
+}
